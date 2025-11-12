6a118ad5a32d36ef1770edffc53412b10f834d0f
David <amoahdavid00@gmail.com>
2025-11-12 02:39:38 +0000

    Update project files

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer () {
+ return (
+         <footer >
+         <p> <span style={{ fontWeight: 'bold', color: 'blue'}}>© 2023 City Lovers</span></p>
+          </footer>
+
+ );
+
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+function MainContent () {
+return (
+<main >
+  <p> <span style={{ fontWeight: 'bolder', textAlign: 'center', color: 'blue'}}>I love to visit New York, Paris, and Tokyo.</span></p>
+</main>
+
+
+);
+
+
+}
+
+export default MainContent;
