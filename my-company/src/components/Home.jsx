836c9f8a534f7f82c404eb69ecc9a5ea9f63f0a3
David <amoahdavid00@gmail.com>
2025-11-14 15:24:05 +0000

    Update project files

--- my-company/src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-return (
-<div style={{ padding: '20px' }}>
-<h1>Welcome to Our Company</h1>
-<p>We are dedicated to delivering excellence in all our services.</p>
-</div>
-);
-}
