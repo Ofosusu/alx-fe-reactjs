304ce98c0b15c45637a4debe9595c0bd2ee9dda4
David <amoahdavid00@gmail.com>
2025-12-15 15:59:56 +0000

    Update project files

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,11 @@
-import Search from "./components/search";
-
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <Search />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
