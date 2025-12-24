d87b4bd0688a9ba0eae57d179877df0202f5d2e1
David <amoahdavid00@gmail.com>
2025-12-24 03:26:50 +0000

    Update project files

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-200">
+      <UserProfile />
+    </div>
+  )
+}
+
+export default App
